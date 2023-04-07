0975f098973fc535168d2fc6009574dc738222e5
AidenRIreland <aidenireland2004@gmail.com>
2023-04-07 18:53:26 -0400

    Finish Utils Class

--- Group Project for Programming 2/Utils.cs
+++ Group Project for Programming 2/Utils.cs
@@ -9,8 +9,23 @@ namespace Group_Project_for_Programming_2
 {
     public static class Utils
     {
-        
-        private static DateTime _time = new DateTime(1_048_000_000);
-        private class 
+        static DayTime _time = new DayTime(1_048_000_000);
+        static Random random = new Random();
+        public static DayTime Time
+        {
+            get => _time += random.Next(1000);
+        }
+        public static DayTime Now
+        {
+            get => _time += 0;
+        }
+
+        public readonly static Dictionary<AccountType, string> ACCOUNT_TYPES =
+        new Dictionary<AccountType, string>
+        {
+            { AccountType.Checking , "CK" },
+            { AccountType.Saving , "SV" },
+            { AccountType.Visa , "VS" }
+        };
     }
 }
