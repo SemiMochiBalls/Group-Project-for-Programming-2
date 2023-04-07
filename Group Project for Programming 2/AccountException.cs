8369614653f2dd5eab0458ecec11e8e7ff73e242
AidenRIreland <aidenireland2004@gmail.com>
2023-04-07 18:59:21 -0400

    Add and finish account exception cclass

---
+++ Group Project for Programming 2/AccountException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_Project_for_Programming_2
+{
+    class AccountException: Exception
+    {
+        public AccountException(ExceptionType reason) : base(reason.ToString()) { }
+    }
+}
