ef5901e3a0de66500355c843ad2feb1306e3a949
AidenRIreland <aidenireland2004@gmail.com>
2023-04-07 19:01:37 -0400

    addeded comments

--- Group Project for Programming 2/AccountException.cs
+++ Group Project for Programming 2/AccountException.cs
@@ -7,7 +7,9 @@ using System.Threading.Tasks;
 namespace Group_Project_for_Programming_2
 {
     class AccountException: Exception
+        //Class is Child of Exception
     {
         public AccountException(ExceptionType reason) : base(reason.ToString()) { }
+        //Returns exception to string when hit
     }
 }
