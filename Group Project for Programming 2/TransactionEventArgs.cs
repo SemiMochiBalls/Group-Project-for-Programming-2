953b0ac055ccc6d0f33984f7e7028a99b2ade69a
SemiMochiBalls <123606903+SemiMochiBalls@users.noreply.github.com>
2023-04-13 19:12:02 -0400

    added TEvent ARgs

---
+++ Group Project for Programming 2/TransactionEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_Project_for_Programming_2
+{
+
+
+    public class TransactionEventArgs : LoginEventArgs
+    {
+        public double Amount { get; }
+
+        public TransactionEventArgs(string name, double amount, bool success) : base(name, success)
+        {
+            Amount = amount;
+        }
+    }
+
+}
