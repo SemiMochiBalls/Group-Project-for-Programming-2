0c8d169ab2986660033228b292771cb38babf0ae
SemiMochiBalls <123606903+SemiMochiBalls@users.noreply.github.com>
2023-04-13 19:11:52 -0400

    added loging even args

---
+++ Group Project for Programming 2/LoginEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_Project_for_Programming_2
+{
+    public class LoginEventArgs : EventArgs
+    {
+        public string Name { get; }
+        public bool Success { get; }
+
+        public LoginEventArgs(string name, bool success)
+        {
+            Name = name;
+            Success = success;
+        }
+    }
+}
