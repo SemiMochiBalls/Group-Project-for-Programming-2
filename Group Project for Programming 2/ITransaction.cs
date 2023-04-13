ee19738b0f9cf2321fceb883cdf5e991f997c77f
SemiMochiBalls <123606903+SemiMochiBalls@users.noreply.github.com>
2023-04-13 19:55:09 -0400

    added class

---
+++ Group Project for Programming 2/ITransaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_Project_for_Programming_2
+{
+    public interface ITransaction
+    {
+        void Withdraw(double amount, Person person);
+        void Deposit(double amount, Person person);
+    }
+
+}
