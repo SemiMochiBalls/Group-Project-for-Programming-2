4355a4f692c8dda0ed4240861bd01ea5c2f28fa3
SemiMochiBalls <123606903+SemiMochiBalls@users.noreply.github.com>
2023-04-13 18:53:26 -0400

    Added transactions struct and logic

---
+++ Group Project for Programming 2/Transaction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_Project_for_Programming_2
+{
+    public struct Transaction
+    {
+        public string AccountNumber { get; }
+        public double Amount { get; }
+        public Person Originator { get; }
+        public DateTime Time { get; }
+
+        public Transaction(string accountNumber, double amount, Person person)
+        {
+            AccountNumber = accountNumber;
+            Amount = amount;
+            Originator = person;
+            Time = Utils.Time;
+        }
+
+        public override string ToString()
+        {
+            return $"Account Number: {AccountNumber}, Name: {Originator.Name}, Amount: {Amount:C}, Time: {Time}";
+        }
+    } 
+    
+}
+
+
